e27b38052b6481754ca486c32ee94ca92ceabef4
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-26 16:38:29 +0200

    add seeder technologies

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,8 +14,16 @@ class TechnologySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $_technologies = ["HTML", "CSS", "SQL", "JavaScript", "PHP", "GIT", "Blade"];
+
+        foreach($_technologies as $_technology) {
+        $technology = new Technology();
+        $technology->label = $_technology;
+        $technology->color = $faker->hexColor() ;
+        
+        $technology->save();
+        }
     }
 }
